5e54c87fe63a382564579aa0e35016cc3ef15fcc
toomium <tom.mittendorf@web.de>
2026-02-01 15:39:55 +0000

    removed debugging dumps

--- tools/fuzzing/ipc/IPCFuzzController.cpp
+++ tools/fuzzing/ipc/IPCFuzzController.cpp
@@ -1565,24 +1565,24 @@ UniquePtr<IPC::Message> IPCFuzzController::replaceIPCMessage(
                            IPC::StringFromIPCMessageType(aMsg->type()),
                            aMsg->header()->payload_size);
     #ifdef FUZZING_SNAPSHOT_LPM
-    dumpIPCMessageToFile(aMsg, mIPCDumpCount, true /* aUseNyx */);
+    // dumpIPCMessageToFile(aMsg, mIPCDumpCount, true /* aUseNyx */);
     UniquePtr<TypedProtobuf> proto = ConvertIPCMessageToProtobuf(aMsg);
     if (! proto) {
       MOZ_FUZZING_NYX_PRINT("Error: Failed to convert msg to proto for seed generation\n");
       return aMsg;
     }
     dumpProtobufMessageToFile(proto, mIPCDumpCount, true);
-    mIPCDumpCount++;
-    UniquePtr<IPC::Message> msg = ConvertProtobufToIPCMessage(proto);
-    if (! msg) {
-      MOZ_FUZZING_NYX_PRINT("Error: Failed to convert msg to proto for seed generation\n");
-      return aMsg;
-    }
-    auto new_size = msg->header()->payload_size;
-    memcpy(msg->header(), aMsg->header(), sizeof(IPC::Message::Header));
-    msg->header()->payload_size = new_size;
-    dumpIPCMessageToFile(msg, mIPCDumpCount, true);
-    MOZ_FUZZING_NYX_PRINT("INFO: [DumpToFile] Message dumped\n");
+    // mIPCDumpCount++;
+    // UniquePtr<IPC::Message> msg = ConvertProtobufToIPCMessage(proto);
+    // if (! msg) {
+    //   MOZ_FUZZING_NYX_PRINT("Error: Failed to convert msg to proto for seed generation\n");
+    //   return aMsg;
+    // }
+    // auto new_size = msg->header()->payload_size;
+    // memcpy(msg->header(), aMsg->header(), sizeof(IPC::Message::Header));
+    // msg->header()->payload_size = new_size;
+    // dumpIPCMessageToFile(msg, mIPCDumpCount, true);
+    // MOZ_FUZZING_NYX_PRINT("INFO: [DumpToFile] Message dumped\n");
     #else
     dumpIPCMessageToFile(aMsg, mIPCDumpCount, true /* aUseNyx */);
     #endif
