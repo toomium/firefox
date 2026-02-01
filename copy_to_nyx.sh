b3fd3bb3e05a8bf4bb5a56e1ec06eddedade8c79
toomium <tom.mittendorf@web.de>
2026-02-01 19:51:47 +0000

    added rel path

--- copy_to_nyx.sh
+++ copy_to_nyx.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -r ~/orion-tom/services/nyx/sharedir/firefox
+rm -r ../orion-tom/services/nyx/sharedir/firefox
 
-cp -r obj-x86_64-pc-linux-gnu/dist/firefox/ ~/orion-tom/services/nyx/sharedir
+cp -r obj-x86_64-pc-linux-gnu/dist/firefox/ ../orion-tom/services/nyx/sharedir
 
-echo "SourceStamp=9f59b559a71685a5078a92af991a211b88988c19" >> ~/orion-tom/services/nyx/sharedir/firefox/platform.ini
+echo "SourceStamp=9f59b559a71685a5078a92af991a211b88988c19" >> ../orion-tom/services/nyx/sharedir/firefox/platform.ini
 
-cat > ~/orion-tom/services/nyx/sharedir/firefox/firefox.fuzzmanagerconf << 'EOF'
+cat > ../orion-tom/services/nyx/sharedir/firefox/firefox.fuzzmanagerconf << 'EOF'
 [Main]
 platform = x86-64
 product = mozilla-central
