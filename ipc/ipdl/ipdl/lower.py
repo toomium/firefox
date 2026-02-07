230df8c6feee9e474d65a8135b0ca56fb0495f76
toomium <tom.mittendorf@web.de>
2026-02-07 18:12:04 +0000

    bug fix

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -4808,7 +4808,7 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
             declstmts = self.makeResolver(md, errfnRecv, routingId=idvar)
 
         # only call recv handler when we do NOT have a fuzz message OR we are not in dryrun mode while fuzzing
-        recvHandler = StmtIf(ExprVar("!msg__.IsFuzzMsg()) || !getenv(\"MOZ_FUZZ_DRYRUN\")"))
+        recvHandler = StmtIf(ExprVar("!msg__.IsFuzzMsg() || !getenv(\"MOZ_FUZZ_DRYRUN\")"))
         recvHandler.addifstmts(self.invokeRecvHandler(md))
         recvHandler.addelsestmt(StmtReturn(_Result.ProcessingError))
 
